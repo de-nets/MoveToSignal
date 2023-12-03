82b5ea2c442f109f9b17cfb36aa2d41ff0618395
Thomas Lüder <t.lueder@nets.de>
2023-12-03 11:59:30 +0100

    Clean up project

--- bin/move_to_signal.dart
+++ bin/move_to_signal.dart
@@ -1,5 +1,3 @@
-import 'package:move_to_signal/move_to_signal.dart' as move_to_signal;
-
 void main(List<String> arguments) {
-  print('Hello world: ${move_to_signal.calculate()}!');
+  print('Hello world: !');
 }
--- test/move_to_signal_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:move_to_signal/move_to_signal.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
