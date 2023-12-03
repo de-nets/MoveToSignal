bbe038f85453b2741814f95a0284fdbee2b28afe
Thomas Lüder <t.lueder@nets.de>
2023-12-03 12:09:03 +0100

    Added code for main function

--- bin/move_to_signal.dart
+++ bin/move_to_signal.dart
@@ -1,3 +1,22 @@
+import 'package:move_to_signal/source_whats_app.dart';
+
 void main(List<String> arguments) {
-  print('Hello world: !');
+  String importSource = 'WhatsApp';
+
+  // Read all arguments
+  for (final argument in arguments) {
+    if (argument.startsWith('--importSource=')) {
+      importSource = argument.split('=').last;
+    }
+  }
+
+  switch (importSource) {
+    case 'WhatsApp':
+      final whatsAppImport = SourceWhatsApp();
+      whatsAppImport.run(arguments);
+
+      break;
+    default:
+      print('Invalid argument --importSource');
+  }
 }
