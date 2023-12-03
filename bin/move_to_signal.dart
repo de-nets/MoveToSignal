da0b7c1774a7319c65e5fb7c8405043a15651ade
Thomas Lüder <t.lueder@nets.de>
2023-12-03 11:55:41 +0100

    dart create move-to-signal

---
+++ bin/move_to_signal.dart
@@ -0,0 +1,5 @@
+import 'package:move_to_signal/move_to_signal.dart' as move_to_signal;
+
+void main(List<String> arguments) {
+  print('Hello world: ${move_to_signal.calculate()}!');
+}
---
+++ test/move_to_signal_test.dart
@@ -0,0 +1,8 @@
+import 'package:move_to_signal/move_to_signal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
